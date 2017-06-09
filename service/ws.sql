36f3c2d998e170158292c76f11aa62fb8c334143
xiangdong <xiangdongdong@chelun.com>
2017-06-09 10:05:55 +0800

    1.更新sql 和 文档

--- service/ws.sql
+++ service/ws.sql
@@ -1,22 +1,22 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat MySQL Data Transfer
 
-Source Server         : 本地
-Source Server Version : 50534
-Source Host           : localhost:3306
-Source Database       : food
+ Source Server         : 本地
+ Source Server Version : 50627
+ Source Host           : localhost
+ Source Database       : ws
 
-Target Server Type    : MYSQL
-Target Server Version : 50534
-File Encoding         : 65001
+ Target Server Version : 50627
+ File Encoding         : utf-8
 
-Date: 2015-10-22 15:39:30
+ Date: 06/09/2017 10:02:08 AM
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for admin
+--  Table structure for `admin`
 -- ----------------------------
 DROP TABLE IF EXISTS `admin`;
 CREATE TABLE `admin` (
@@ -27,15 +27,10 @@ CREATE TABLE `admin` (
   `login_time` int(11) DEFAULT NULL COMMENT '登录时间',
   `ctime` int(11) DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of admin
--- ----------------------------
-INSERT INTO `admin` VALUES ('13', 'test', '51abb9636078defbf888d8457a7c76f85c8f114c', '1', '1', '1');
-
--- ----------------------------
--- Table structure for category
+--  Table structure for `category`
 -- ----------------------------
 DROP TABLE IF EXISTS `category`;
 CREATE TABLE `category` (
@@ -46,12 +41,26 @@ CREATE TABLE `category` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of category
+--  Table structure for `city`
 -- ----------------------------
-INSERT INTO `category` VALUES ('1', '川菜', '1');
+DROP TABLE IF EXISTS `city`;
+CREATE TABLE `city` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL COMMENT '城市名称',
+  `imgs` text NOT NULL COMMENT '图片',
+  `desc` text COMMENT '描述',
+  `status` int(11) NOT NULL DEFAULT '1' COMMENT '状态 0 下线 1 正常',
+  `must_food` text NOT NULL COMMENT '必吃美食',
+  `must_view` text NOT NULL COMMENT '必去景点',
+  `guide` text NOT NULL COMMENT '城市指南',
+  `play` text NOT NULL COMMENT '怎么玩',
+  `code` varchar(10) DEFAULT NULL COMMENT '邮编',
+  `ctime` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for comment
+--  Table structure for `comment`
 -- ----------------------------
 DROP TABLE IF EXISTS `comment`;
 CREATE TABLE `comment` (
@@ -64,11 +73,7 @@ CREATE TABLE `comment` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of comment
--- ----------------------------
-
--- ----------------------------
--- Table structure for food
+--  Table structure for `food`
 -- ----------------------------
 DROP TABLE IF EXISTS `food`;
 CREATE TABLE `food` (
@@ -84,11 +89,22 @@ CREATE TABLE `food` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of food
+--  Table structure for `home_page`
 -- ----------------------------
+DROP TABLE IF EXISTS `home_page`;
+CREATE TABLE `home_page` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `slogan` text NOT NULL COMMENT '首页标语',
+  `video_url` text NOT NULL COMMENT '视频链接',
+  `banner` text NOT NULL COMMENT 'bannner 图片',
+  `season_view` text NOT NULL COMMENT '当季景点',
+  `recommend_view` text NOT NULL COMMENT '推荐景点',
+  `update_time` int(11) NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for like
+--  Table structure for `like`
 -- ----------------------------
 DROP TABLE IF EXISTS `like`;
 CREATE TABLE `like` (
@@ -100,11 +116,7 @@ CREATE TABLE `like` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of like
--- ----------------------------
-
--- ----------------------------
--- Table structure for login_log
+--  Table structure for `login_log`
 -- ----------------------------
 DROP TABLE IF EXISTS `login_log`;
 CREATE TABLE `login_log` (
@@ -113,11 +125,22 @@ CREATE TABLE `login_log` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of login_log
+--  Table structure for `point`
 -- ----------------------------
+DROP TABLE IF EXISTS `point`;
+CREATE TABLE `point` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `city_id` int(11) NOT NULL COMMENT '所属城市',
+  `type` varchar(100) NOT NULL COMMENT '类型',
+  `name` varchar(255) NOT NULL COMMENT '名称',
+  `status` int(11) NOT NULL DEFAULT '1' COMMENT '状态 0下线 1 正常',
+  `obj` text NOT NULL COMMENT '字段内容',
+  `ctime` int(11) NOT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for user
+--  Table structure for `user`
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
@@ -134,6 +157,4 @@ CREATE TABLE `user` (
   PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of user
--- ----------------------------
+SET FOREIGN_KEY_CHECKS = 1;
